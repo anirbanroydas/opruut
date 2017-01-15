0abc4c47a3ed6c4adb106659363342a1bb830399
Anirban Roy Das <anirban.nick@gmail.com>
2017-01-15 23:10:55 +0530

    added new migration

---
+++ database/migrations/2017_01_05_190048_create_favorites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('favorites', function (Blueprint $table) {
+            Schema::dropIfExists('favorites');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
