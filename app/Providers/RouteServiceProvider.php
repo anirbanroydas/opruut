1f7402a8194686a2e73f945136495a6fc0865df4
Anirban Roy Das <anirban.nick@gmail.com>
2017-01-15 22:45:07 +0530

    added RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -16,6 +16,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected $namespace = 'App\Http\Controllers';
 
+    
+
     /**
      * Define your route model bindings, pattern filters, etc.
      *
