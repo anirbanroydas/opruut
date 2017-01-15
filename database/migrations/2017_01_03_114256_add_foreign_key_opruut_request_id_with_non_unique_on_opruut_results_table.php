c60677f8a1a5c36f91518a51a59689437866a843
Anirban Roy Das <anirban.nick@gmail.com>
2017-01-15 23:10:12 +0530

    added new migration

---
+++ database/migrations/2017_01_03_114256_add_foreign_key_opruut_request_id_with_non_unique_on_opruut_results_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyOpruutRequestIdWithNonUniqueOnOpruutResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('opruut_results', function (Blueprint $table) {
+            $table->integer('opruut_request_id')->unsigned()->index();
+
+            $table->foreign('opruut_request_id')
+                  ->references('id')->on('opruut_requests')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('opruut_results', function (Blueprint $table) {
+            $table->dropForeign('opruut_results_opruut_request_id_foreign');
+            $table->dropColumn('opruut_request_id');
+        });
+    }
+}
