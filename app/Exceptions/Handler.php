f8d04e15777d300b19af545f2476a0d9104e9bff
Anirban Roy Das <anirban.nick@gmail.com>
2017-01-15 21:56:27 +0530

    modified Exception Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,8 @@ use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -44,6 +46,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof MethodNotAllowedHttpException && env('APP_DEBUG') === false) {
+
+            return abort('404');
+        }
+
         return parent::render($request, $exception);
     }
 
