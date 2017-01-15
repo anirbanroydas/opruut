c34c4b4f1d8d9ab6726ffb1db75b14d37b535b8d
Anirban Roy Das <anirban.nick@gmail.com>
2017-01-15 23:09:57 +0530

    added new migration

---
+++ database/migrations/2017_01_03_092008_update_column_travel_time_from_float_to_json_on_opruut_results_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateColumnTravelTimeFromFloatToJsonOnOpruutResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('opruut_results', function (Blueprint $table) {
+            $table->dropColumn('travel_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('opruut_results', function (Blueprint $table) {
+            
+            $table->json('travel_time')->after('travel_distance');
+        });
+    }
+}
