df0a0637897ad5680d23879556e97b1ffa13ea99
Anirban Roy Das <anirban.nick@gmail.com>
2017-01-15 23:28:40 +0530

    added resources assets js actions index.js

---
+++ resources/assets/js/actions/index.js
@@ -0,0 +1,104 @@
+// import all the constants
+// import { AUTH_USER, AUTH_ERROR, SIGN_OUT_USER } from './AuthActions';
+// import { FIND_OPRUUT, GET_OPRUUT, GET_OPRUUT_SUCCESS, GET_OPRUUT_FAILURE, INCOMPLETE_REQUEST_DATA, COMPLETE_REQUEST_DATA, 
+// CLEAR_OPRUUT, UPDATE_PREFERENCE, UPDATE_RIDE_TIME, UPDATE_SOURCE_STATION_ID, UPDATE_SOURCE_STATION_NAME, 
+// UPDATE_DESTINATION_STATION_ID, UPDATE_DESTINATION_STATION_NAME, REMOVE_DESTINATION_ID, REMOVE_SOURCE_ID,
+//  NO_PREFERENCE, TIME_PREFERENCE, 
+// COMFORT_PREFERENCE, BOTH_PREFERENCE, RIDE_NOW } from './OpruutRequestActions';
+// import { UPDATE_GLOBAL_REQUESTS, REQUEST_OPRUUTS, RECEIVE_OPRUUTS, RECEIVE_OPRUUTS_FAILURE, ADD_OPRUUT_TO_LIST } from './OpruutListActions';
+// import { RECEIVE_STATION_LIST, RECEIVE_STATION_LIST_FAILURE , UPDATE_SEARCH_CITY, REMOVE_SEARCH_CITY} from './SearchActions';
+// import { FETCH_FAVORITED_OPRUUTS } from './FavoritesActions';
+// import { REQUEST_LIVESTREAM, RECEIVE_LIVESTREAM, RECEIVE_LIVESTREAM_FAILURE } from './LivestreamActions';
+
+
+
+// // import all action creartors
+// import { signUpUser, signInUser, signOutUser } from './AuthActions';
+// import { fetchOpruutIfNeeded, clearOpruutIfNeeded, updatePreference, updateRideTime, updateSourceStation, updateDestinationStation } from './OpruutRequestActions';
+// import { fetchOpruutListIfNeeded, addOpruutResultToOpruutList } from './OpruutListActions';
+// import { fetchStationListIfNeeded, updateSearchCity } from './SearchActions';
+// import { fetchFavoritedOpruut } from './FavoritesActions';
+// import { fetchLivestreamIfNeeded } from './LivestreamActions';
+
+
+
+export * from './AuthActions';
+export * from './OpruutRequestActions';
+export * from './OpruutListActions';
+export * from './SearchActions';
+export * from './FavoritesActions';
+export * from './LivestreamActions';
+export * from './BrowserHistoryActions';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
